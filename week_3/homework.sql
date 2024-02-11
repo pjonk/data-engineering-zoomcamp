e659b931a9d75d2e076eca4d74e1fbd341143eeb
Patrick Jonk <patrick@MacBook-Air-van-Patrick.local>
2024-02-11 23:05:24 +0100

    Updated hw 3

--- week_3/homework.sql
+++ week_3/homework.sql
@@ -9,16 +9,35 @@ CREATE OR REPLACE EXTERNAL TABLE `dtc-de-course-409315.ny_taxi.green_taxi_2022_e
 CREATE OR REPLACE TABLE dtc-de-course-409315.ny_taxi.green_taxi_2022 AS
 SELECT * FROM dtc-de-course-409315.ny_taxi.green_taxi_2022_external;
 
+--- Question 1
 --- Number of rows
 SELECT COUNT(*) FROM dtc-de-course-409315.ny_taxi.green_taxi_2022; 
 
-SELECT DISTINCT(VendorID)
+--- Question 2
+SELECT COUNT(DISTINCT(VendorID))
 FROM dtc-de-course-409315.ny_taxi.green_taxi_2022;
 
-SELECT DISTINCT(VendorID)
+SELECT COUNT(DISTINCT(VendorID))
 FROM dtc-de-course-409315.ny_taxi.green_taxi_2022_external;
 
+--- Question 3
 --- zero fare amount
 SELECT COUNT(*)
 FROM dtc-de-course-409315.ny_taxi.green_taxi_2022_external
-WHERE fare_amount=0.0
+WHERE fare_amount=0.0;
+
+--- Question 4
+-- Create a partitioned and clustered table
+CREATE OR REPLACE TABLE dtc-de-course-409315.ny_taxi.green_taxi_2022_partition_cluster 
+PARTITION BY lpep_pickup_date
+CLUSTER BY VendorID AS
+SELECT * FROM dtc-de-course-409315.ny_taxi.green_taxi_2022_external;
+
+--- Question 5
+SELECT COUNT(DISTINCT(VendorID))
+FROM dtc-de-course-409315.ny_taxi.green_taxi_2022
+WHERE DATE(lpep_pickup_date) BETWEEN '2022-06-01' AND '2022-06-30';
+
+SELECT COUNT(DISTINCT(VendorID))
+FROM dtc-de-course-409315.ny_taxi.green_taxi_2022_partition_cluster
+WHERE DATE(lpep_pickup_date) BETWEEN '2022-06-01' AND '2022-06-30';
